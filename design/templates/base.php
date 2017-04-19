fd24f5cf5b56a139cf5b649504e2b4f753f8ef69
Nicolas de la Sizeranne <nicolasdechastaing@gmail.com>
2017-04-19 21:19:18 +0200

    maj mise en page

--- design/templates/base.php
+++ design/templates/base.php
@@ -1,7 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: nicol_000
- * Date: 19/04/2017
- * Time: 21:18
- */
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet/less" type="text/css" href="design/less/main.less">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.7.2/less.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <title>Nora Project</title>
+</head>
+<body>
