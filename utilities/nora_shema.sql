d7843884399b4d581d193fd9a49812243372a07c
Nicolas de la Sizeranne <nicolasdechastaing@gmail.com>
2017-04-25 23:22:13 +0200

    modification BDD + insert USER

--- utilities/nora_shema.sql
+++ utilities/nora_shema.sql
@@ -1,85 +1,35 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Client :  127.0.0.1
--- Généré le :  Mar 18 Avril 2017 à 21:57
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+CREATE TABLE fiche__action
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id_auteur INT(11) NOT NULL,
+  id_editeur INT(11),
+  date DATE NOT NULL,
+  contenu TEXT NOT NULL,
+  id_pj INT(11),
+  type VARCHAR(255) NOT NULL
+);
+CREATE TABLE fiche__entreprise
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(535) NOT NULL,
+  mail VARCHAR(535),
+  adresse TEXT,
+  telephone VARCHAR(255)
+);
+CREATE UNIQUE INDEX nom ON fiche__entreprise (nom);
+CREATE TABLE fiche__piecejointe
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  lien_pj TEXT NOT NULL
+);
+CREATE TABLE users
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL,
+  password TEXT NOT NULL,
+  id_entreprise INT(11),
+  statut INT(11)
+);
+CREATE UNIQUE INDEX username ON users (username);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de données :  `nora`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `fiche__action`
---
-
-CREATE TABLE IF NOT EXISTS `fiche__action` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_auteur` int(11) NOT NULL,
-  `id_editeur` int(11) DEFAULT NULL,
-  `date` date NOT NULL,
-  `contenu` text NOT NULL,
-  `id_pj` int(11) DEFAULT NULL,
-  `type` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `fiche__entreprise`
---
-
-CREATE TABLE IF NOT EXISTS `fiche__entreprise` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(535) NOT NULL,
-  `mail` varchar(535) DEFAULT NULL,
-  `adresse` text,
-  `telephone` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `nom` (`nom`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `fiche__piecejointe`
---
-
-CREATE TABLE IF NOT EXISTS `fiche__piecejointe` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `lien_pj` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` text NOT NULL,
-  `statut` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO nora.users (username, password, id_entreprise, statut) VALUES ('Test', '098f6bcd4621d373cade4e832627b4f6', null, 0);
