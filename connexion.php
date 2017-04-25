237bd08445a1dec9c84955a96581ac07179c4a94
Nicolas de la Sizeranne <nicolasdechastaing@gmail.com>
2017-04-25 23:07:14 +0200

    traitement formulaire connexion

--- connexion.php
+++ connexion.php
@@ -5,14 +5,30 @@
  * Date: 19/04/2017
  * Time: 21:19
  */
-    include('design/templates/base.php');
+
 
     if(isset($_POST['username'])|| isset($_POST['password'])){
+        $bdd = new PDO('mysql:host=localhost;dbname=nora;charset=utf8', 'root', '');
+        $req = $bdd->prepare('SELECT COUNT(*) FROM users WHERE username = ? AND password = ?');
+        $req->execute(array($_POST['username'], md5($_POST['password']) ));
+
+        $nb = $req->fetchColumn();
+
+        if($nb == 1){
+            header('Location : /nora/index.php');
+            exit();
+        }
+        else{
+            $message = "Bad credentials";
+        }
+
 
     }
 
+include('design/templates/base.php');
 
 ?>
+
 
 <div class="card card-teal" style="margin-left: 25%; margin-right: 25%; margin-top: 10%;">
     <h1 class="header">Connexion à Nora</h1>
