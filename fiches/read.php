41047b9457c46a067b65fb613bd25057a3f0cdf7
Romain <barnoud.r@gmail.com>
2017-05-01 22:36:53 +0200

    read.php modifié avec affichage tableau pas encore opérationnel

--- fiches/read.php
+++ fiches/read.php
@@ -5,8 +5,26 @@
  * Date: 26/04/2017
  * Time: 21:50
  */
+require(__DIR__.'/../base.php');
+if(empty($_SESSION['user'])) header('Location: '.__DIR__.'/../connexion.php');
 
+$query1= $bdd-> query("SELECT id,date,contenu,type FROM fiche_action "); //pas de jointure
+$action = $query1->fetch();
 
+$query2 = $bdd-> query("
+SELECT fiche_action.id_auteur,users.username 
+FROM fiche_action,users 
+WHERE fiche_action.id_auteur = users.id "); //jointure pour récuperer le nom de l'employé qui l'a créé
+$auteur = $query2->fetch();
+
+$query3 = $bdd-> query("
+SELECT fiche_action.id_auteur,users.username 
+FROM fiche_action,users 
+WHERE fiche_action.id_auteur = users.id "); //jointure pour récuperer le nom de l'employé qui l'a modifié
+$editeur = $query3->fetch();
+
+$query4 = $bdd-> query("SELECT id,lien_pj FROM `fiche_piecejointe` ");
+$piecejointe = $query4->fetch();
 
 
 ?>
@@ -18,30 +36,55 @@
     <body>
         <h1> coucou </h1>
         <?php
-        // on affiche les actions de la fiche
-        $query = $bdd-> query("SELECT * FROM `fiche_action` ");
-        $action = $query->fetch();
+        if($_SESSION['user']->getStatut()==0){?>
 
-        echo "L'action n°"." a été créée par ". " le ".$action['date']. ". Elle a été modifié le "." par ". "."; // date de modification à ajouter
-        echo " Voici les différences pièces jointes " ; // faire une liste des pièces jointes
-        // comment récuperer le nom de Id_auteur et Id_editeur ? jointure ?
-        // modifier et faire un tableau avec toutes les infos
-
-
-        // si on est un employé
-        if($_SESSION['user']->getStatut()==0){
-            echo "<form method='POST' action='fiche/edit.php'>
-            <a href='fiche/edit.php' target='_blank'> <input type='submit' value='modification fiche' > </a>
-            </form>";
-
-            echo "<form method='POST' action='fiche/create.php'>
-            <a href='fiche/create.php' target='_blank'> <input type='submit' value='Ajouter action' > </a>
-            </form>\";
-            ";
+            <form method='POST' action='fiche/edit.php'>
+                <a href='fiche/edit.php' target='_blank'> <input type='submit' value='Modification action' > </a> </br>
+                <a href='fiche/create.php' target='_blank'> <input type='submit' value='Ajouter action' > </a>
+            </form>
+        <?php
         }
-        else{
-            // on fait rien
+        ?>
+        <?php
+            while($action){
+
+        ?>
+        <table>
+            <caption>Fiche de l'action n°</caption> // mettre la dernière action en haut
+            <thead>
+                <tr>
+                    <th>Date de création</th>
+                    <th>Créer par</th>
+                    <th>Modifier par</th>
+                    <th>Date de dernière modification</th> // ajouter champ BDD
+                    <th>Contenu</th>
+                    <th>Pièces jointes</th>
+                    <th>Type d'action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><?php echo(action['date'])?></td>
+                    <td><?php echo(auteur['users.username'])?></td>
+                    <td><?php echo(action['date'])?></td>
+                    <td><?php echo(action['date'])?></td>
+                    <td><?php echo(action['contenu'])?></td>
+                    <td><?php while($piecejointe){
+                        echo"
+                        <li>
+                            <a href='article.php?id=".$piecejointe['lien_pj']."'>"
+                            .$piecejointe['lien_pj']
+                            ."</a>
+                        </li>";
+                        }
+                        ?></td>
+                    <td><?php echo(action['type'])?></td>
+                </tr>
+            </tbody>
+        </table>
+        <?php
         }
         ?>
     </body>
 </html>
+
